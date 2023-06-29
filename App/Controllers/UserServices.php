ad40d0d092d2b7bc527667a9e17273dbf549e367
Leonardo <leonardoaugusto1000a@gmail.com>
2023-06-29 17:43:06 -0300

    criando as funções de get/post/update/delete

---
+++ App/Controllers/UserServices.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\UserServices;
+
+class UserServices
+{
+    //  Pegando os dados dos usuarios
+    public function get()
+    {
+
+    }
+
+    // Inserindo usuarios
+    public function post()
+    {
+
+    }
+
+    // Alterar usuarios
+    public function update()
+    {
+
+    }
+
+    // Deletando usuarios
+    public function delete()
+    {
+
+    }
+    
+}
--- App/models/User.php
+++ App/models/User.php
@@ -10,6 +10,23 @@ class User
     public static function get(int $id)
     {
         $conn = new \PDO(DBDRIVE. ':host='.DBHOST.';dbname='.DBNAME, DBUSER,DBPASS);
-    }
 
+        // Selecionando todos os dados do DB e utilizando as var já definidas em config
+        $sql = 'SELECT * FROM' .self::$table. 'WHERE id = :id'; // Obs: Já que são var static precisamos usar .self
+
+        // Statements serve para evitar ataques de SQL INJECTION.
+        $statement = $conn->prepare($sql);
+        $statement->bindValue(':id', $id);
+        $statement->execute();
+
+        // Verificando se houve retorno na consulta
+        if($statement-> rowCount() > 0)
+        {
+
+        } else{
+            throw new \Exception("Usuário não encontrado!");
+            
+        }
+
+    }
 }
